23464ba141078bfca486796585abfbf07cfc0ada
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-09-08 18:20:08 +0200

    - change `Unlawful` to be a `def`, rather than `abbrev` - fix all proofs to consider new abstraction barriers

--- CompPoly/Unlawful.lean
+++ CompPoly/Unlawful.lean
@@ -1,7 +1,6 @@
 import CompPoly.CMvMonomial
 import CompPoly.Wheels
 import Mathlib.Algebra.Lie.OfAssociative
-import Std.Classes.Ord.Vector
 
 attribute [local instance 5] instDecidableEqOfLawfulBEq
 
@@ -12,12 +11,52 @@ open Std
 /--
   Polynomial in `n` variables with coefficients in `R`.
 -/
-abbrev Unlawful (n : ℕ) (R : Type) : Type :=
-  Std.ExtTreeMap (CMvMonomial n) R
+@[grind =]
+def Unlawful (n : ℕ) (R : Type) : Type :=
+  Std.ExtTreeMap (CMvMonomial n) R (Ord.compare (α := CMvMonomial n))
+
+variable {n : ℕ} {R : Type}
+
+instance : EmptyCollection (Unlawful n R) := ⟨(∅ : Std.ExtTreeMap (CMvMonomial n) R compare)⟩
+
+instance : Singleton (MonoR n R) (Unlawful n R) :=
+  inferInstanceAs (Singleton (MonoR n R) (Std.ExtTreeMap (CMvMonomial n) R compare))
+
+instance : Insert (MonoR n R) (Unlawful n R) :=
+  inferInstanceAs (Insert (MonoR n R) (Std.ExtTreeMap (CMvMonomial n) R compare))
+
+instance : LawfulSingleton (MonoR n R) (Unlawful n R) :=
+  inferInstanceAs (LawfulSingleton (MonoR n R) (Std.ExtTreeMap (CMvMonomial n) R compare))
+
+instance : Membership (CMvMonomial n) (Unlawful n R) :=
+  inferInstanceAs (Membership (CMvMonomial n) (Std.ExtTreeMap (CMvMonomial n) R compare))
+
+@[default_instance]
+instance (priority := high) : GetElem (Unlawful n R) (CMvMonomial n) R (fun lp m ↦ m ∈ lp) :=
+  inferInstanceAs
+    (GetElem (Std.ExtTreeMap (CMvMonomial n) R compare) (CMvMonomial n) R (fun lp m ↦ m ∈ lp))
+
+instance : GetElem? (Unlawful n R) (CMvMonomial n) R (fun lp m ↦ m ∈ lp) :=
+  inferInstanceAs
+    (GetElem? (Std.ExtTreeMap (CMvMonomial n) R compare) (CMvMonomial n) R (fun lp m ↦ m ∈ lp))
+
+instance : LawfulGetElem (Unlawful n R) (CMvMonomial n) R (fun lp m ↦ m ∈ lp) :=
+  inferInstanceAs
+    (LawfulGetElem (Std.ExtTreeMap (CMvMonomial n) R compare) (CMvMonomial n) R (fun lp m ↦ m ∈ lp))
 
 namespace Unlawful
 
+attribute [grind ext] Std.ExtTreeMap.ext_getElem?
+
+@[ext, grind ext]
+lemma ext_getElem? {n R} {t₁ t₂ : Unlawful n R}
+  (h : ∀ (k : CMvMonomial n), t₁[k]? = t₂[k]?) : t₁ = t₂ :=
+  Std.ExtTreeMap.ext_getElem? h
+
 variable {n : ℕ} {R : Type}
+
+@[simp, grind =]
+def ofList (l : List (CMvMonomial n × R)) : Unlawful n R := ExtTreeMap.ofList l compare
 
 def extend (n' : ℕ) (p : Unlawful n R) : Unlawful (n ⊔ n') R :=
   .ofList (p.keys.map (CMvMonomial.extend n') |>.zip p.values)
@@ -41,9 +80,9 @@ instance [Repr R] : Repr (Unlawful n R) where
       ⟨λ (m, c) => repr c ++ " * " ++ repr m⟩
     @Std.Format.joinSep _ toFormat p.toList " + "
 
-@[grind=]
+@[grind =]
 def C [BEq R] [LawfulBEq R] [Zero R] (c : R) : Unlawful n R :=
-  if c = 0 then Std.ExtTreeMap.empty else .ofList [MonoR.C c]
+  if c = 0 then ∅ else .ofList [MonoR.C c]
 
 section
 
@@ -57,18 +96,17 @@ instance : OfNat (Unlawful n R) 0 := ⟨C 0⟩
 
 instance [NatCast R] [NeZero m] : OfNat (Unlawful n R) m := ⟨C m⟩
 
-@[simp, grind _=_]
+@[simp, grind =]
 lemma C_zero : C (n := n) (0 : R) = 0 := rfl
 
 end
 
-@[simp, grind]
+@[simp, grind =]
 lemma C_zero' : C (n := n) (0 : ℕ) = 0 := rfl
 
-@[simp, grind _=_]
+@[simp, grind =]
 lemma zero_eq_zero : (Zero.zero : R) = 0 := rfl
 
-@[grind←]
 lemma zero_eq_empty [BEq R] [LawfulBEq R] : (0 : Unlawful n R) = ∅ := by unfold_projs; simp [C]
 
 @[simp, grind]
@@ -79,10 +117,10 @@ section
 variable [BEq R] [LawfulBEq R]
 
 @[simp, grind]
-lemma not_mem_zero : x ∉ (0 : Unlawful n R) := by grind
+lemma not_mem_zero : x ∉ (0 : Unlawful n R) := by rw [←C_zero]; grind
 
 @[simp, grind]
-lemma isNoZeroCoef_zero : isNoZeroCoef (n := n) (R := R) 0 := fun _ ↦ by simp
+lemma isNoZeroCoef_zero : isNoZeroCoef (n := n) (R := R) 0 := by rw [←C_zero]; grind
 
 end
 
@@ -98,10 +136,10 @@ protected lemma grind_add_skip [Add R] {p₁ p₂ : Unlawful n R} :
   p₁ + p₂ = p₁.mergeWith (fun _ c₁ c₂ ↦ c₁ + c₂) p₂ := rfl
 
 def addMonoR [Add R] (p : Unlawful n R) (term : MonoR n R) : Unlawful n R :=
-  p + (ExtTreeMap.ofList [term] : Unlawful n R)
+  p + .ofList [term]
 
 def mul₀ [Mul R] (t : MonoR n R) (p : Unlawful n R) : Unlawful n R :=
-  ExtTreeMap.ofList (p.toList.map fun (k, v) ↦ (t.1*k, t.2*v))
+  .ofList (p.toList.map fun (k, v) ↦ (t.1+k, t.2*v))
 
 attribute [grind=] ExtTreeMap.ofList_eq_empty_iff List.map_eq_nil_iff ExtTreeMap.toList_eq_nil_iff
 
@@ -113,7 +151,7 @@ lemma mul₀_zero [Zero R] [BEq R] [LawfulBEq R] [Mul R] {t : MonoR n R} : mul�
 def mul [Mul R] [Add R] [Zero R] [BEq R] [LawfulBEq R] (p₁ p₂ : Unlawful n R) : Unlawful n R :=
   p₁.foldl (init := 0)
     fun p m₁ c₁ ↦
-      (p₂.foldl (init := 0) fun p' m₂ c₂ ↦ {(m₁ * m₂, c₁ * c₂)} + p') + p
+      (p₂.foldl (init := 0) fun p' m₂ c₂ ↦ {(m₁ + m₂, c₁ * c₂)} + p') + p
 
 instance [BEq R] [LawfulBEq R] [Mul R] [Add R] [Zero R] : Mul (Unlawful n R) := ⟨mul⟩
 
@@ -141,50 +179,30 @@ instance instDecidableEq [DecidableEq R] : DecidableEq (Unlawful n R) := fun x y
 def coeff {R : Type} {n : ℕ} [Zero R] (m : CMvMonomial n) (p : Unlawful n R) : R :=
   p[m]?.getD 0
 
+
+@[simp, grind =]
 lemma filter_get {R : Type} [BEq R] [LawfulBEq R] {v : R} {m : CMvMonomial n} (a : Unlawful n R) :
     (ExtTreeMap.filter (fun _ c => c != v) a)[m]?.getD v = a[m]?.getD v := by
-  by_cases h : m ∈ a
-  · by_cases h' : a[m] = v
-    · erw [ExtTreeMap.filter_not_pred h (by simp [h'])]
-      have : a[m]? = .some v := by
-        aesop
-      rw [this]
-      simp
-    · erw [ExtTreeMap.filter_mem h (by simp [h'])]
-      have : a[m]?.getD v = a[m] := by
-        have : a[m]? = some a[m] := by
-          simp [h]
-        rw [this]
-        simp
-      rw [this]
-      simp
-  · simp [h, ExtTreeMap.filter_not_mem h]
+  grind
 
 lemma add_getD? [CommSemiring R] {m : CMvMonomial n} {p q : Unlawful n R} :
   (p.add q)[m]?.getD 0 = p[m]?.getD 0 + q[m]?.getD 0
 := by
   rw [Unlawful.add]
   by_cases in_p : m ∈ p <;> by_cases in_q : m ∈ q
-  · simp [ExtTreeMap.mergeWith₀ in_p in_q]
-    by_cases sum_0 : p[m] + q[m] != 0
-      <;> by_cases pm_0 : p[m] = 0 <;> by_cases qm_0 : q[m] = 0
-      <;> grind [add_comm]
-  · simp [ExtTreeMap.mergeWith₁ in_p in_q]
-    by_cases p[m] = 0
-    · aesop
-    · rw [ExtTreeMap.mem_iff_isSome_getElem?, Option.isSome_iff_exists] at in_p
-      rcases in_p with ⟨c₁, in_p⟩
-      simp [in_p]
-      by_cases c₁_eq_0 : c₁ = 0 <;> simp [c₁_eq_0, in_q]
-  · simp [ExtTreeMap.mergeWith₂ in_p in_q]
-    by_cases q[m] = 0
-    · aesop
-    · rw [ExtTreeMap.mem_iff_isSome_getElem?, Option.isSome_iff_exists] at in_q
-      rcases in_q with ⟨c₁, in_q⟩
-      simp [in_q]
-      by_cases c₁_eq_0 : c₁ = 0 <;> simp [c₁_eq_0, in_p]
-  · simp [ExtTreeMap.mergeWith₃ in_p in_q]
-    aesop
+  · erw [ExtTreeMap.mergeWith₀ in_p in_q, Option.getD_some]
+    change p[m] + q[m] = p[m]?.getD 0 + q[m]?.getD 0
+    grind
+  · erw [ExtTreeMap.mergeWith₁ in_p in_q, show q[m]?.getD 0 = 0 by grind, add_zero]
+    grind
+  · erw [ExtTreeMap.mergeWith₂ in_p in_q, show p[m]?.getD 0 = 0 by grind, zero_add]
+    grind
+  · erw [
+      ExtTreeMap.mergeWith₃ in_p in_q,
+      show p[m]?.getD 0 = 0 by grind,
+      show q[m]?.getD 0 = 0 by grind
+    ]
+    grind
 
 end Unlawful
 
